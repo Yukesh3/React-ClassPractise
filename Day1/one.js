d0390063467db67db400c12a392029ead2b998b3
Yukesh3 <yukesh.fsd@gmail.com>
2022-05-10 01:42:56 +0530

    Done

---
+++ Day1/one.js
@@ -0,0 +1,33 @@
+//React Js =>
+//React is a JavaScript library for building user interfaces. React is used to build single-page applications. React allows us to create reusable UI components.
+
+//React vs Angular vs Vue =>
+//React is a UI library, Angular is a fully-fledged front-end framework, while Vue. js is a progressive framework. They can be used almost interchangeably to build front-end applications, but they're not 100 percent the same.
+
+// Features of React:
+// JSX (JavaScript Syntax Extension)
+// Virtual DOM.
+// One-way data binding.
+// Performance.
+// Extensions.
+// Conditional statements.
+// Components.
+// Simplicity.
+
+//Framework vs Library =>
+//The main difference between frameworks and libraries is regarding control of the application. In the case of a library, you are in full control. You implement the library when and where required. The situation is quite the opposite in the case of a framework - the framework allows you to add a little code here and there for customization but it will decide whether or not to use all of your code.
+
+//Components =>
+//Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML. Components come in two types, Class components and Function components
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <h1>Welcome 🔥</h1>
+)
+
+//CRA =>
+//Create React App is a command line tool which creates a starter project for you.
