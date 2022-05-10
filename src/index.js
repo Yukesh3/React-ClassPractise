10f2a78529d3b53ce592518e6d5d06d2eed8f56c
Yukesh3 <yukesh.fsd@gmail.com>
2022-05-10 23:41:56 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,76 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ClassComponent from './ClassComponent';
-import Functional from './Functional';
-import './index.css'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-<>
-
-    <h1>Styling using Functional and Class Component</h1>
-    <Functional/>
-    <ClassComponent/>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css'
+import Appbar from  './components/Appbar';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Home from './pages/Home'
+import Login from './pages/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+const root=ReactDOM.createRoot(document.getElementById('root'));
 
 
-</>    
+root.render(  
+    <>
+    <BrowserRouter>
+    <Appbar/>
+
+    <Routes>
+
+    <Route path='/' element={<Home/>}/>
+    <Route path='/login' element={<Login/>}/>
+
+    </Routes>
+    
+    </BrowserRouter>
+      
+
+    
+            
+           
+                  
+    </>        
+            );
 
 
+//
+// const Time=()=>{  
+//     const time=new Date().toLocaleTimeString();
+//     return <h1 className='time'>Time: {time} </h1>
+// }
+
+// const value=true;
+
+// const names='Vinay';
+
+// const arr1=['Vasanthi','Triveni','Durgadevi','Manoj'];
 
 
-
-
-
-
-
-
-
-
-
-/* <div>
-
- 
-
-
-<h1>Self Intro 😁</h1>
-
-<details>
-
-<summary>Summary</summary>
-
-<pre>
-
-<h2><mark>Name:</mark> Yukesh K </h2> <br/>
-
-<h2><mark>Date of Birth:</mark>  31-05-1999</h2><br/>
-
-<h2>I am from Tamilnadu. I did B.E Mechanical Engineering in MAM School Of Engineering college in 2020.
-After that i was worked <br/>in mechanical company name was Youngshin India Private Limited around 11 months
-with the role of quality inspection. During <br/>pandemic breakdown i left that job. Now i am currently doing 
-<mark> Full Stack Developement</mark> course bacause i want to change my career. </h2><br/>
-
-<a href="mailto:yukesh.fsd@gmail.com" rel="noreferrer"  target="_blank"> 
-<img src='email.png' width="30px" alt='Email'></img>
-</a> &nbsp; 
-
-<a href="https://github.com/Yukesh3" rel="noreferrer" target="_blank"> 
-<img src='GitHub.png' width="30px" alt='Git'></img>
-</a>  
-
-</pre>
-
-</details>
-
-
-
-
-
-</div> */
-
-
-
-
-
-);
-
+// // Hooks use karenge || 
